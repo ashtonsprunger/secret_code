b1d966fe5b50d670b7b9a92db1652f6b0116fea1
ashtonsprunger <sprungerashton@gmail.com>
2021-08-21 17:35:36 -0400

    remove number

--- script.js
+++ script.js
@@ -1,58 +1,65 @@
-const inputEl = document.getElementById('input')
-const numberEl = document.getElementById('number')
-const outputEl = document.getElementById('output')
-const convertEl = document.getElementById('convert')
-const toggleEl = document.getElementById('toggle')
-const copyEl = document.getElementById('copy')
+const inputEl = document.getElementById("input");
+const numberEl = document.getElementById("number");
+const outputEl = document.getElementById("output");
+const convertEl = document.getElementById("convert");
+const toggleEl = document.getElementById("toggle");
+const copyEl = document.getElementById("copy");
 
-let ENCODE = '0'
-let DECODE = '1'
+let ENCODE = "0";
+let DECODE = "1";
 let MODE = ENCODE;
 
 function copy_text(text) {
-    //Before we copy, we are going to select the text.
-    var selection = window.getSelection();
-    var range = document.createRange();
-    range.selectNodeContents(text);
-    selection.removeAllRanges();
-    selection.addRange(range);
-    //add to clipboard.
-    document.execCommand('copy');
+  //Before we copy, we are going to select the text.
+  var selection = window.getSelection();
+  var range = document.createRange();
+  range.selectNodeContents(text);
+  selection.removeAllRanges();
+  selection.addRange(range);
+  //add to clipboard.
+  document.execCommand("copy");
 }
 
-inputEl.addEventListener('focus', () => {
-    inputEl.select()
-})
+inputEl.addEventListener("focus", () => {
+  inputEl.select();
+});
 
-numberEl.addEventListener('focus', () => {
-    numberEl.select()
-})
+// numberEl.addEventListener('focus', () => {
+//     numberEl.select()
+// })
 
-toggleEl.addEventListener('click', () => {
-    MODE = MODE == ENCODE ? DECODE : ENCODE
-    convertEl.innerText = MODE == ENCODE ? 'ENCODE!' : 'DECODE!'
-})
+toggleEl.addEventListener("click", () => {
+  MODE = MODE == ENCODE ? DECODE : ENCODE;
+  convertEl.innerText = MODE == ENCODE ? "ENCODE!" : "DECODE!";
+});
 
-convertEl.addEventListener('click', () => {
-    if(inputEl.value.trim()){
-
-        let outputText
-        if(MODE == ENCODE){
-            outputText = encode(inputEl.value.trim(), Number.parseInt(numberEl.value))
-        }else{
-            outputText = decode(inputEl.value.trim(), Number.parseInt(numberEl.value))
-        }
-        outputEl.innerText = outputText
-        if(outputEl.innerText.trim()){
-            outputEl.style.border = '3px solid white'
-            copyEl.style.display = 'block'
-        }else{
-            outputEl.style.border = 'none'
-            copyEl.style.display = 'none'
-        }
+convertEl.addEventListener("click", () => {
+  if (inputEl.value.trim()) {
+    let outputText;
+    if (MODE == ENCODE) {
+      outputText = encode(
+        inputEl.value.trim(),
+        // Number.parseInt(numberEl.value)
+        1
+      );
+    } else {
+      outputText = decode(
+        inputEl.value.trim(),
+        // Number.parseInt(numberEl.value)
+        1
+      );
     }
-    })
+    outputEl.innerText = outputText;
+    if (outputEl.innerText.trim()) {
+      outputEl.style.border = "3px solid white";
+      copyEl.style.display = "block";
+    } else {
+      outputEl.style.border = "none";
+      copyEl.style.display = "none";
+    }
+  }
+});
 
-copyEl.addEventListener('click', () => {
-    copy_text(outputEl)
-})
+copyEl.addEventListener("click", () => {
+  copy_text(outputEl);
+});
